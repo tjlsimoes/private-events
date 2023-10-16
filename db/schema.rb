b0f03001d40fd6b0318884cafbaefb55de58faa2
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-16 18:16:12 +0100

    Add title to events database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_165831) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_16_171527) do
   create_table "events", force: :cascade do |t|
     t.datetime "when"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
