a6055e7a6c1c464afdeb6074d611d086b01e5b12
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-18 18:49:53 +0100

    Add events#create and corresponding route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,26 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: :index
 	def index
 		@events = Event.all
 	end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    # @event = Event.new(event_params)
+    @event = current_user.build(event_params)
+  
+    if @event.save
+        redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :when, :creator_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }, sign_out_via: [:get, :post]
   resources :users, only: :show
-	resources :events, only: :index
+	resources :events, only: [:index, :new, :create]
 end
