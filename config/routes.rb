12b1217532dc063cf453527c74b53186405e1c5c
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-18 11:47:21 +0100

    Set User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "events#index"
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }, sign_out_via: [:get, :post]
 	resources :events, only: :index
 end
