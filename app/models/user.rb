18e962c568a9b2abfe26f867a4fe98a4cee28619
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-19 11:04:27 +0100

    Enable attendee-event :through assocation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+         :recoverable, :rememberable, :validatabl
+
+	has_many :event_attendances, foreign_key: :attendee_id
+	has_many :attended_events, through: :event_attendances # possible need of source here
+
+	has_many :created_events, foreign_key: "creator_id", class_name: "Event"
   # has_many :attended_events, foreign_key: "attendee_id", class_name: "Event"
 end
